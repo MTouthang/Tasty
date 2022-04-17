7fbdc934463ecf25f2f4e125e5295599a4d2411c
Mamang touthang <huebart6@gmail.com>
2022-04-18 01:08:37 +0530

    Popular component

---
+++ src/component/Veggie.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export function Veggie(props) {
+    return (
+        <div>Popular</div>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Popular from '../component/Popular'
+import { Veggie } from '../component/Veggie'
+
+function Home() {
+  return (
+    <div>
+        <Veggie/>
+        <Popular/>
+    </div>
+  )
+}
+
+export default Home
